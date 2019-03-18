02d937ab1b00d29e20a84251aaebf3296610450f
jokabe <okabej3@hawaii.edu>
2019-03-17 16:23:39 -1000

    Created Device list popup and "Add a Device" button
    - Created a popup to show device list
    - Added a "Add a device" button, which redirects to "Add a Device" page

---
+++ TenDen/app/src/main/java/com/teamMJW/tenden/AddDevice.java
@@ -0,0 +1,28 @@
+package com.teamMJW.tenden;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class AddDevice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_device);
+
+        goBackToPreviousPage();
+    }
+
+    //For testing purposes......Go back to the Main Page when "Back" button is clicked
+    private void goBackToPreviousPage() {
+        Button mainPageButton = findViewById(R.id.backMainPage);
+        mainPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
