a65e05160980b1fedabcac0fc62a7307a34dd424
jokabe <okabej3@hawaii.edu>
2019-04-08 05:55:29 -1000

    Emulator Test Mode
    - "App Not Working" is displayed -> cause is from using Gson for the data storage in the emulators (works with non-emulator phone)
    - Added in boolean variable "emulatorMode" to prevent the Gson code from running -> Set to True for emulator runs and Set to False for non-emulator use

--- TenDen/app/src/main/java/com/teamMJW/tenden/AddDevice.java
+++ TenDen/app/src/main/java/com/teamMJW/tenden/AddDevice.java
@@ -15,6 +15,8 @@ import java.net.InetAddress;
 
 public class AddDevice extends AppCompatActivity {
 
+    private boolean emulatorMode = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,56 +27,78 @@ public class AddDevice extends AppCompatActivity {
     }
 
     public void setDeviceInformation() {
-        //get device id and power status data from SharedPreferences (stores primitive data)
-        SharedPreferences s = getSharedPreferences("APPDATA", Context.MODE_PRIVATE);
-        Gson gson = new Gson();
-        String json = s.getString("Bulb", null);
-        Device bulb = gson.fromJson(json, Device.class);
-        String id = bulb.getBulbId();
-        String powerState = bulb.getCurrentPowerStatus();
-        String brightness = bulb.getCurrentBrightness();
-        String ct = bulb.getCurrentCT();
-        InetAddress ip = bulb.getCurrentIP();
 
-        TextView deviceid = findViewById(R.id.device_id);
-        TextView devicePower = findViewById(R.id.device_power);
-        TextView deviceBrightness = findViewById(R.id.device_brightness);
-        TextView deviceColorTemp = findViewById(R.id.device_colortemp);
-        TextView deviceIp = findViewById(R.id.device_ip);
+        if(emulatorMode) {
 
-        if(id == null) {
+            TextView deviceid = findViewById(R.id.device_id);
+            TextView devicePower = findViewById(R.id.device_power);
+            TextView deviceBrightness = findViewById(R.id.device_brightness);
+            TextView deviceColorTemp = findViewById(R.id.device_colortemp);
+            TextView deviceIp = findViewById(R.id.device_ip);
+
             deviceid.setText("Device ID: ?");
-        } else {
-            deviceid.setText("Device ID: " + id);
-            deviceid.setTextSize(10);
-        }
 
-        if(powerState == null) {
             devicePower.setText("Power: ?");
-        } else {
-            devicePower.setText("Power: " + powerState);
-            devicePower.setTextSize(10);
-        }
 
-        if(brightness == null) {
             deviceBrightness.setText("Brightness: ?");
-        } else {
-            deviceBrightness.setText("Brightness: " + brightness);
-            deviceBrightness.setTextSize(10);
-        }
 
-        if(ct == null) {
             deviceColorTemp.setText("Color Temperature: ?");
-        } else {
-            deviceColorTemp.setText("Color Temperature: " + ct);
-            deviceColorTemp.setTextSize(10);
-        }
 
-        if(ip == null) {
             deviceIp.setText("IP Address: ?");
+
+
         } else {
-            deviceIp.setText("IP Address: " + ip);
-            deviceIp.setTextSize(10);
+            //get device id and power status data from SharedPreferences (stores primitive data)
+            SharedPreferences s = getSharedPreferences("APPDATA", Context.MODE_PRIVATE);
+            Gson gson = new Gson();
+            String json = s.getString("Bulb", null);
+            Device bulb = gson.fromJson(json, Device.class);
+            String id = bulb.getBulbId();
+            String powerState = bulb.getCurrentPowerStatus();
+            String brightness = bulb.getCurrentBrightness();
+            String ct = bulb.getCurrentCT();
+            InetAddress ip = bulb.getCurrentIP();
+
+            TextView deviceid = findViewById(R.id.device_id);
+            TextView devicePower = findViewById(R.id.device_power);
+            TextView deviceBrightness = findViewById(R.id.device_brightness);
+            TextView deviceColorTemp = findViewById(R.id.device_colortemp);
+            TextView deviceIp = findViewById(R.id.device_ip);
+
+            if (id == null) {
+                deviceid.setText("Device ID: ?");
+            } else {
+                deviceid.setText("Device ID: " + id);
+                deviceid.setTextSize(10);
+            }
+
+            if (powerState == null) {
+                devicePower.setText("Power: ?");
+            } else {
+                devicePower.setText("Power: " + powerState);
+                devicePower.setTextSize(10);
+            }
+
+            if (brightness == null) {
+                deviceBrightness.setText("Brightness: ?");
+            } else {
+                deviceBrightness.setText("Brightness: " + brightness);
+                deviceBrightness.setTextSize(10);
+            }
+
+            if (ct == null) {
+                deviceColorTemp.setText("Color Temperature: ?");
+            } else {
+                deviceColorTemp.setText("Color Temperature: " + ct);
+                deviceColorTemp.setTextSize(10);
+            }
+
+            if (ip == null) {
+                deviceIp.setText("IP Address: ?");
+            } else {
+                deviceIp.setText("IP Address: " + ip);
+                deviceIp.setTextSize(10);
+            }
         }
 
     }
